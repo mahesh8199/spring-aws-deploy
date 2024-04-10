e25f5a430a2707033ffd48ef52afe3c76a43df1f
Mahesh <maheshkurapati1@gmail.com>
2024-04-10 08:19:00 -0400

    added aws deploy code into git

---
+++ src/main/java/com/aws/springawsdeploy/SpringAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.aws.springawsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsDeployApplication.class, args);
+	}
+
+}
